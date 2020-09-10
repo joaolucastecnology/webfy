a0f48d676ebce943144162112a3d9312d98b084c
Joao Lucas Melo <joaosouza1135@gmail.com>
2020-09-09 21:56:38 -0300

    fix fields and responsives

--- projetos-pessoais/conversor/script/leve_x.js
+++ projetos-pessoais/conversor/script/leve_x.js
@@ -10,7 +10,7 @@ function calcularPromocao() {
 
     let calc_pague = (calc_leve * pague_y);
 
-    preco_final = document.getElementById('preco_prom').value = calc_pague.toFixed(2);
+    preco_final = document.getElementById('preco_prom').value = `R$: ${calc_pague.toFixed(2)}`;
 
     if (preco_partida === '' | leve_x === '' | pague_y === '' | preco_final === '') {
         alert('Para Efetuar o calculo, por favor preencha Todos os campos.')
