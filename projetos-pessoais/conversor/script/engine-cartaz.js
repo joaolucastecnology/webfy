06b8b52d98149d5055a5503bf8a1721f01c88ec9
Joao Lucas Melo <joaosouza1135@gmail.com>
2020-09-22 22:10:35 -0300

    adicionando novas funcionalidades

--- projetos-pessoais/conversor/script/engine-cartaz.js
+++ projetos-pessoais/conversor/script/engine-cartaz.js
@@ -1,27 +1,41 @@
 function texto1() {
     let texto1 = document.getElementById('texto1').value
-    console.log(texto1)
 
     let recebeTexto1 = document.getElementById('recebe-texto1').innerHTML = texto1
 }
 
 function texto2() {
     let texto2 = document.getElementById('texto2').value
-    console.log(texto2)
 
     let recebeTexto2 = document.getElementById('recebe-texto2').innerHTML = texto2
 }
 
 function medida() {
     let medida = document.getElementById('medida').value
-    console.log(medida)
 
     let medidaProduto = document.getElementById('medida-produto').innerHTML = medida
 }
 
-function precoProduto() {
-    let precoProduto = document.getElementById('preco-produto').value
-    console.log(precoProduto)
 
-    let PrecoProduto1 = document.getElementById('recebe-preco-value').innerHTML = precoProduto
+function precoProdutoDe(result) {
+    let precoProdutoDe2 = document.getElementById('preco-produto-de').value
+    result = document.getElementById('recebe-preco-value-de').innerHTML = `${precoProdutoDe2}`
 }
+
+
+
+function precoProdutoPor() {
+    let precoProduto = document.getElementById('preco-produto-por').value
+    let PrecoProduto1 = document.getElementById('recebe-preco-value').innerHTML = `${precoProduto}`
+}
+
+function data1() {
+    str = 'Válido Até : '
+    sep = ' De Hoje a '
+    campoDataP = document.getElementById('data1').value
+    console.log(campoDataP)
+
+    document.getElementById('campoValidade').innerHTML = `${str} ${sep} ${campoDataP}`
+}
+
+
