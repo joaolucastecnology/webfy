7644ce90411faf7ff06a3f43d85bb31ee22a33fa
Joao Lucas Melo <joaosouza1135@gmail.com>
2021-03-12 21:52:38 -0300

    add ;

--- Projetos/souzlinks/js/validation.js
+++ Projetos/souzlinks/js/validation.js
@@ -2,23 +2,23 @@ function msgDia() {
     var msg = document.getElementById('msg');
     var data = new Date();
     var hora = data.getHours();
-    var minuto = data.getMinutes()
-    var hora = `${data.getHours()} : ${data.getMinutes()}`
-    var horas = document.getElementById('hora')
+    var minuto = data.getMinutes();
+    var hora = `${data.getHours()} : ${data.getMinutes()}`;
+    var horas = document.getElementById('hora');
 
     setInterval(function () {
         horas.innerHTML = ((new Date).toLocaleString().substr(11, 8));  
     }, 1000);
-    msg.style.textAlign = `center`
+    msg.style.textAlign = `center`;
     
 
     if (hora >= 0 && hora <= 12) {
-            msg.innerHTML += ` Olá, Bom Dia `
+            msg.innerHTML += ` Olá, Bom Dia `;
     }
      else if (hora >=12 && hora <= 18) {
-        msg.innerHTML += ` Olá, Boa Tarde `
+        msg.innerHTML += ` Olá, Boa Tarde `;
     }
      else {
-        msg.innerHTML += ` Olá, Boa Noite `
+        msg.innerHTML += ` Olá, Boa Noite `;
     }
 }
