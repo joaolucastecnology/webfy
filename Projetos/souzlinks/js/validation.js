0ca5e1f28fb037c89c76c2559b7af314161544c3
Joao Lucas Melo <joaosouza1135@gmail.com>
2021-03-13 15:07:17 -0300

    fix msg dia

--- Projetos/souzlinks/js/validation.js
+++ Projetos/souzlinks/js/validation.js
@@ -1,8 +1,7 @@
 function msgDia() {
     var msg = document.getElementById('msg');
     var data = new Date();
-    var horax = data.getHours();
-    var minuto = data.getMinutes();
+    var msgDia = data.getHours();
     var hora = `${data.getHours()} : ${data.getMinutes()}`;
     var horas = document.getElementById('hora');
 
@@ -12,10 +11,10 @@ function msgDia() {
     msg.style.textAlign = `center`;
     
 
-    if (horax >= 0 && horax <= 12) {
+    if (msgDia >= 0 && msgDia <= 12) {
             msg.innerHTML += ` Olá, Bom Dia `;
     }
-     else if (horax >=12 && horax <= 18) {
+     else if (msgDia >=12 && msgDia <= 18) {
         msg.innerHTML += ` Olá, Boa Tarde `;
     }
      else {
