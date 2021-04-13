c0e0dbc0195005f04b6ca47939a142852adf8382
Joao Lucas Melo <joaosouza1135@gmail.com>
2021-04-13 14:34:08 -0300

    UPDATE API ARRAY

--- Projetos/cotacao-moeda/script/main.js
+++ Projetos/cotacao-moeda/script/main.js
@@ -2,49 +2,49 @@ function init() {
   $.ajax({
       type: "GET",
       dataType: "JSON",
-      url: "https://economia.awesomeapi.com.br/json/all",
+      url: "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,ETH-BRL,XRP-BRL",
       success: function(data) {
           let dolar = "";
-          dolar += `Codigo: ${data['USD']['code']} <br/>`;
-          dolar += `Nome: ${data['USD']['name']} <br/>`;
-          dolar += `Compra: ${data['USD']['bid']} <br/>`;
-          dolar += `Venda: ${data['USD']['ask']}`;
+          dolar += `Codigo: ${data['USDBRL']['code']} <br/>`;
+          dolar += `Nome: ${data['USDBRL']['name']} <br/>`;
+          dolar += `Compra: ${data['USDBRL']['bid']} <br/>`;
+          dolar += `Venda: ${data['USDBRL']['ask']}`;
 
           let conteudoDolar = document.getElementById('dolar');
           conteudoDolar.innerHTML = dolar;
 
           let euro = "";
-          euro += `Codigo: ${data['EUR']['code']} <br/>`;
-          euro += `Nome: ${data['EUR']['name']} <br/>`;
-          euro += `Compra: ${data['EUR']['bid']} <br/>`;
-          euro += `Venda: ${data['EUR']['ask']}`;
+          euro += `Codigo: ${data['EURBRL']['code']} <br/>`;
+          euro += `Nome: ${data['EURBRL']['name']} <br/>`;
+          euro += `Compra: ${data['EURBRL']['bid']} <br/>`;
+          euro += `Venda: ${data['EURBRL']['ask']}`;
 
           let conteudoEuro = document.getElementById('euro');
           conteudoEuro.innerHTML = euro;
 
           let btc = "";
-          btc += `Codigo: ${data['BTC']['code']} <br/>`;
-          btc += `Nome: ${data['BTC']['name']} <br/>`;
-          btc += `Compra: ${data['BTC']['bid']} <br/>`;
-          btc += `Venda: ${data['BTC']['ask']}`;
+          btc += `Codigo: ${data['BTCBRL']['code']} <br/>`;
+          btc += `Nome: ${data['BTCBRL']['name']} <br/>`;
+          btc += `Compra: ${data['BTCBRL']['bid']} <br/>`;
+          btc += `Venda: ${data['BTCBRL']['ask']}`;
 
           let conteudoBtc = document.getElementById('bitcoin');
           conteudoBtc.innerHTML = btc;
 
           let eth = "";
-          eth += `Codigo: ${data['ETH']['code']} <br/>`;
-          eth += `Nome: ${data['ETH']['name']} <br/>`;
-          eth += `Compra: ${data['ETH']['bid']} <br/>`;
-          eth += `Venda: ${data['ETH']['ask']}`;
+          eth += `Codigo: ${data['ETHBRL']['code']} <br/>`;
+          eth += `Nome: ${data['ETHBRL']['name']} <br/>`;
+          eth += `Compra: ${data['ETHBRL']['bid']} <br/>`;
+          eth += `Venda: ${data['ETHBRL']['ask']}`;
 
           let conteudoEth = document.getElementById('eth');
           conteudoEth.innerHTML = eth
 
           let xrp = "";
-          xrp += `Codigo: ${data['XRP']['code']} <br/>`;
-          xrp += `Nome: ${data['XRP']['name']} <br/>`;
-          xrp += `Compra: ${data['XRP']['bid']} <br/>`;
-          xrp += `Venda: ${data['XRP']['ask']}`;
+          xrp += `Codigo: ${data['XRPBRL']['code']} <br/>`;
+          xrp += `Nome: ${data['XRPBRL']['name']} <br/>`;
+          xrp += `Compra: ${data['XRPBRL']['bid']} <br/>`;
+          xrp += `Venda: ${data['XRPBRL']['ask']}`;
 
           conteudoXrp = document.getElementById('xrp');
           conteudoXrp.innerHTML = xrp;
