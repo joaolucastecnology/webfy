93c0f79ce6f509a5ba3e73a20d75cadf77180f1e
Joao Lucas Melo <joaosouza1135@gmail.com>
2021-06-17 21:34:37 -0300

    add new home page

--- Projetos/jlmslink/script/engine.js
+++ Projetos/jlmslink/script/engine.js
@@ -2,17 +2,15 @@ function msgDia() {
     var msg = document.getElementById('msg');
     var data = new Date();
     var msgDia = data.getHours();
-    var hora = `${data.getHours()} : ${data.getMinutes()}`;
     var horas = document.getElementById('hora');
 
     setInterval(function () {
         horas.innerHTML = ((new Date).toLocaleString().substr(11, 8));  
     }, 1000);
-    msg.style.textAlign = `center`;
     
 
     if (msgDia >= 0 && msgDia <= 12) {
-            msg.innerHTML += ` Olá, Bom Dia `;
+            msg.innerHTML += `Olá, Bom Dia `;
     }
      else if (msgDia >=12 && msgDia <= 18) {
         msg.innerHTML += ` Olá, Boa Tarde `;
